db3bcf161179a6f2fff98232c3254b7b665fec2e
alec <alexeialvarez4@gmail.com>
2021-04-02 10:23:34 -0600

    create main section

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,15 @@
 const $iconMenu = document.getElementById('menu-icon'),
-  $navbar = document.getElementById('navbar');
+  $navbar = document.getElementById('navbar'),
+  $searchIcon = document.querySelector('.search-icon');
 
 function showMenu() {
   $iconMenu.classList.toggle('is-active');
   $navbar.classList.toggle('is-active');
 }
+
+function showSearchInput() {
+  window.search.classList.toggle('is-active');
+}
+
+$searchIcon.addEventListener('click', showSearchInput);
 $iconMenu.addEventListener('click', showMenu);
